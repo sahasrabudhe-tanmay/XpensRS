8b6ab3bcb40a3737f15c30c4e790d96bc9a3476d
Tanmay Sahasrabudhe <tanmay.sahasrabudhe@gmail.com>
2018-12-07 13:21:13 +0530

    Added interface for service

--- src/main/java/com/xpens/service/DayService.java
+++ src/main/java/com/xpens/service/DayService.java
@@ -1,36 +1,18 @@
 package com.xpens.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.xpens.model.Day;
-import com.xpens.repository.DayRepository;
 
 @Service
-public class DayService {
-	
-	@Autowired
-	DayRepository dayRepository;
+public interface DayService {
 
-	public List<Day> getAllDays() {
-		List<Day> days = new ArrayList<Day>();
-		dayRepository.findAll().forEach(days::add);
-		return days;
-	}
+	public List<Day> getAllDays();
 	
-	public Day getDay(String id) {
-		return dayRepository.findById(id).orElse(null);
-	}
+	public Day getDay(String id);
 	
-	public void postDay(Day day) {
-		day.setTotalExpense("0");
-		day.getExpenses().forEach(expense -> {
-			day.setTotalExpense(Integer.toString(Integer.parseInt(day.getTotalExpense()) + Integer.parseInt(expense.getCost())));
-		});
-		dayRepository.save(day);
-	}
+	public void postDay(Day day);
 
 }
---
+++ src/main/java/com/xpens/service/DayServiceImpl.java
@@ -0,0 +1,34 @@
+package com.xpens.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.xpens.model.Day;
+import com.xpens.repository.DayRepository;
+
+public class DayServiceImpl implements DayService {
+
+	@Autowired
+	DayRepository dayRepository;
+
+	public List<Day> getAllDays() {
+		List<Day> days = new ArrayList<Day>();
+		dayRepository.findAll().forEach(days::add);
+		return days;
+	}
+	
+	public Day getDay(String id) {
+		return dayRepository.findById(id).orElse(null);
+	}
+	
+	public void postDay(Day day) {
+		day.setTotalExpense("0");
+		day.getExpenses().forEach(expense -> {
+			day.setTotalExpense(Integer.toString(Integer.parseInt(day.getTotalExpense()) + Integer.parseInt(expense.getCost())));
+		});
+		dayRepository.save(day);
+	}
+	
+}
